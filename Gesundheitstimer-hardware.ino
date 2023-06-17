5be715a075e176a57fc6d6b33d7e4586854b4ec8
hyperbel <hyperbel@users.noreply.github.com>
2023-06-17 17:04:36 +0200

    poti code

--- Gesundheitstimer-hardware.ino
+++ Gesundheitstimer-hardware.ino
@@ -1,5 +1,6 @@
-//Datei erstellen mit den variablen onst char* ssid = ""; const char* password = "";
-#include "wlan_zugang.h"
+//Datei erstellen mit den variablen 
+const char* ssid = "jugendhackt"; const char* password = "jugendhackt23";
+//#include "wlan_zugang.h"
 #include <WiFi.h>
 #include "netzwerk.hpp"
 #include "ScaleReader.hpp"
@@ -7,10 +8,14 @@
 ScaleReader* scaleReader;
 netzwerk netzwerksteuerung{};
 
+const int potiPin = 34;
+int potiValue = 0;
+
 void setup(){
   Serial.begin(115200);
   Serial.println();
   Serial.println("Starting...");
+
 
   scaleReader = new ScaleReader();
 
@@ -22,10 +27,18 @@ void setup(){
   }
 
   Serial.println("Connected to WiFi");
+  
+
 }
 
 void loop(){
+  
   float zustand_in_g = scaleReader->readScale();
   netzwerksteuerung.Sendewasserstand(zustand_in_g);
-  delay(5000);
+  delay(0);
+
+  potiValue = analogRead(potiPin);
+  Serial.print(potiValue / 4095.0 * 100);
+  Serial.println(" %");
+  delay(10);
 }
