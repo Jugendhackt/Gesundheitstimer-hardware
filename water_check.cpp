2d286f5540ab75548d5a75b5911a341902a24699
Erase01 <lucajulien16@gmx.de>
2023-06-17 17:54:35 +0200

    added water_check.cpp

---
+++ water_check.cpp
@@ -0,0 +1,31 @@
+#include "water_check.hpp"
+
+water_check::water_check() {
+  // Beginn des trinkintervalls
+  startMillis = millis();
+}
+
+timerstatus water_check::water_timer(double menge_in_g) {
+  if(menge_in_g < 1) {
+    return keine_Flasche_vorhanden;}
+  if(menge_in_g < 460){
+    return Flasche_ist_leer;}
+  auto aktuelle_Zeit = millis();
+  if(letzte_menge - menge_in_g > zeitintervall) {
+    startMillis = aktuelle_Zeit;
+    letzte_menge = menge_in_g;
+    return genug_getrunken;}
+  if(startMillis + zeitintervall < aktuelle_Zeit){
+    return Schlage_Alarm;}
+  return trinken_nicht_noetig;
+}
+void water_check::reset(double menge_in_g){
+  startMillis = millis();
+  letzte_menge = menge_in_g;
+}
+
+void water_check::change_settings(double zielmenge, double zeit) {
+  zielmenge_in_g = zielmenge * 1000;
+  // erhalten der zeit größe (min)
+  zeitintervall = (unsigned int)zeit * 60 * 1000;
+  }
